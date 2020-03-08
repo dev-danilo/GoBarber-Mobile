85971744cc42a708dac8a4737517ba8372e961bc
Danilo Pereira <danilopx1@gmail.com>
2020-03-08 14:37:45 -0300

    Config Redux

---
+++ src/store/modules/user/sagas.js
@@ -0,0 +1,31 @@
+import {Alert} from 'react-native';
+import {takeLatest, call, put, all} from 'redux-saga/effects';
+
+import api from '~/services/api';
+
+import {updateProfileSuccess, updateProfileFailure} from './actions';
+
+export function* updateProfile({payload}) {
+  try {
+    const {name, email, avatar_id, ...rest} = payload.data;
+    console.tron.log(payload.data);
+    const profile = {
+      name,
+      email,
+      avatar_id,
+      ...(rest.oldPassword ? rest : {}),
+    };
+
+    const response = yield call(api.put, 'users', profile);
+    Alert.alert('Sucesso', 'Perfil atualizado com sucesso 🤪');
+    yield put(updateProfileSuccess(response.data));
+  } catch (err) {
+    Alert.alert(
+      'Falha na atualização',
+      'Houve um erro na atualização do perfil, verifique seus dados😭',
+    );
+    yield put(updateProfileFailure());
+  }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
